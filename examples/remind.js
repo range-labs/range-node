ef82037462193aec2faf51bbbd54cfa0e856e6e3
Dan Pupius <dan@range.co>
2019-10-28 16:01:01 -0700

    Support AddSuggestion API and add an example

---
+++ examples/remind.js
@@ -0,0 +1,62 @@
+// Adds a suggestion to remind you to read a link.
+//
+// Usage:
+//  export RANGE_ACCESS_KEY=....
+//  node remind.js https://blog.remote.com/why-you-should-be-doing-async-work/
+
+const https = require('https');
+const Range = require('../lib/range');
+
+const htmlURL = process.argv[2];
+
+console.log('Fetching', htmlURL);
+const req = https.get(htmlURL, res => {
+  res.setEncoding('utf8');
+  let body = '';
+  res.on('data', data => (body += data));
+  res.on('end', () => {
+    if (res.statusCode !== 200) {
+      fail(`status ${res.statusCode}`);
+      return;
+    }
+    const payload = parse(body);
+    new Range()
+      .addSuggestion(payload)
+      .then(data => {
+        console.log('Suggestion made');
+      })
+      .catch(fail);
+  });
+});
+req.on('error', fail);
+req.end();
+
+function parse(body) {
+  let title;
+  const matches = body.match(/<title[^>]*>(.*)<\/title>/);
+  if (matches && matches[1]) {
+    title = matches[1];
+  } else {
+    title = htmlURL;
+  }
+
+  return {
+    snippet_type: 1,
+    dedupe_strategy: 'UPSERT_PENDING',
+    reason: 'ASSIGNED',
+    is_future: true,
+    attachment: {
+      source_id: htmlURL.replace(/[^a-zA-Z0-9]/g, ''),
+      type: 'LINK',
+      provider: 'readinglist',
+      provider_name: 'Reading List',
+      name: title,
+      html_url: htmlURL,
+    },
+  };
+}
+
+function fail(err) {
+  console.error('Error:', err);
+  process.exit(1);
+}
